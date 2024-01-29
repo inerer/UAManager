9fbfeaa3635ef15f42433b625008dde2e2be4daf
arshi <arshinov04@gmail.com>
2024-01-29 19:41:43 +0300

    new update

--- ClientLauncher/MainWindow.xaml.cs
+++ ClientLauncher/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClientLauncher.Pages;
 
 namespace ClientLauncher;
 
@@ -19,5 +20,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        MainFrame.Navigate(new SettingsPage());
     }
 }
--- UAM.Core/Installer/Installer.cs
+++ UAM.Core/Installer/Installer.cs
@@ -7,18 +7,18 @@ namespace UAM.Core.Installer;
 
 public class Installer
 {
-    private ApiUpdate _apiUpdate = new ApiUpdate();
+    private readonly ApiUpdate _apiUpdate = new ApiUpdate();
 
     public async Task Install()
     {
-        // var arguments = Environment.GetCommandLineArgs();
-        //
-        // if (arguments.Length < 2)
-        //     throw new ArgumentNullException("version not specified");
+        var arguments = Environment.GetCommandLineArgs();
 
-        await _apiUpdate.GetUpdate("1.0.0");
+        if (arguments.Length < 2)
+            throw new ArgumentNullException("version not specified");
 
-        var path = "C:\\Users\\arshi\\RiderProjects\\UAManager\\ClientLauncher\\bin\\Release\\net8.0-windows";
+        await _apiUpdate.GetUpdate(arguments[1]);
+
+        var path = Environment.CurrentDirectory;
 
         var files = Directory.GetFiles(path);
 
@@ -26,7 +26,10 @@ public class Installer
 
         foreach (var file in files)
         {
-            if (file.EndsWith("ClientLauncher.exe"))
+            if (file.EndsWith(".exe"))
+                continue;
+
+            if (file.EndsWith(".dll"))
                 continue;
 
             File.Delete(file);
@@ -45,6 +48,42 @@ public class Installer
             Directory.Delete(directory);
         }
 
-        ZipFile.ExtractToDirectory("updates/1.0.0", path);
+        ZipFile.ExtractToDirectory("updates/1.0.0.zip", path);
+    }
+
+    public async Task InstallLastUpdate()
+    {
+        await _apiUpdate.GetLastUpdate();
+        var path = Environment.CurrentDirectory;
+
+        var files = Directory.GetFiles(path);
+
+        var directories = Directory.GetDirectories(path);
+
+        foreach (var file in files)
+        {
+            if (file.EndsWith(".exe"))
+                continue;
+
+            if (file.EndsWith(".dll"))
+                continue;
+
+            File.Delete(file);
+        }
+
+        foreach (var directory in directories)
+        {
+            if (directory.EndsWith("updates"))
+                continue;
+
+            foreach (var file in Directory.GetFiles(directory))
+            {
+                File.Delete(file);
+            }
+
+            Directory.Delete(directory);
+        }
+
+        ZipFile.ExtractToDirectory("updates/1.0.0.zip", path);
     }
 }
